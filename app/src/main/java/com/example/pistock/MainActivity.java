ce098542682b4060badad5383839082c9bc7d1c5
Aditya simant <adityasimant88@gmail.com>
2022-01-23 17:46:58 +0530

    1. Resolved the retro import dependencies error 2. all buttons reset

--- app/src/main/java/com/example/pistock/MainActivity.java
+++ app/src/main/java/com/example/pistock/MainActivity.java
@@ -16,29 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button;
-        EditText editText;
-        ImageButton imgBtn;
 
-        button = findViewById(R.id.btnSearch);
-        editText = findViewById(R.id.etSearch);
-        imgBtn = findViewById(R.id.imgInfo);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String searchString = editText.getText().toString();
-
-                Toast.makeText(getApplicationContext(),searchString,Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        imgBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Info pannel",Toast.LENGTH_SHORT).show();
-            }
-        });
 
 
 
